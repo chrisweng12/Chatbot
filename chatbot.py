6d47e18604330d470dd0e4bed14edbe83bfef60e
wengweicheng <chris86112299.flg06g@nctu.edu.tw>
2020-11-12 13:08:00 +0800

    Debugging with cleaning dictionary

--- chatbot.py
+++ chatbot.py
@@ -18,7 +18,7 @@ id_to_line = {}
 for line in dat_lines:
     newline = line.split(' +++$+++ ')
     if len(newline) == 5:
-        id_to_line[newline[0]] = newline[-1]
+        id_to_line[newline[0]] = newline[4]
 
 # function for iterating certain amount keys and values
 def readDict(dictionary,N):
@@ -28,11 +28,10 @@ def readDict(dictionary,N):
 
 # setting up a list conversation
 conversation_id = []
-for conv in dat_convs:
-    newconv = conv.split(' +++$+++ ')[-1]
-    tempconv = newconv[1:-1].replace("'", "").replace(" ", "")
+for conv in dat_convs[:-1]:
+    tempconv = conv.split(' +++$+++ ')[-1][1:-1].replace("'", "").replace(" ", "")
     conversation_id.append(tempconv.split(','))
-conversation_id.remove(['']) # cleaning out empty rows
+# conversation_id.remove(['']) # cleaning out empty rows
 
 '''
 backup information for the conversation id, 
@@ -54,7 +53,7 @@ def cleanText(text):
     text = re.sub(r"he's", "he is", text)
     text = re.sub(r"she's", "she is", text)
     text = re.sub(r"it's", "it is", text)
-    text = re.sub(r"I'm", "I am", text)
+    text = re.sub(r"i'm", "i am", text)
     text = re.sub(r"that's", "that is", text)
     text = re.sub(r"what's", "what is", text)
     text = re.sub(r"where's", "where is", text)
@@ -66,7 +65,7 @@ def cleanText(text):
     text = re.sub(r"\'ve", " have", text)
     text = re.sub(r"\'re", " are", text)
     text = re.sub(r"\'d", " would", text)
-    text = re.sub(r"-()\"#@;:<>{}+=-|.?]", "", text)
+    text = re.sub(r"[-()\"#/@;:<>{}+=~|.?,]", "", text)
     return text
 
 # cleaning up the question
@@ -98,16 +97,23 @@ for answer in cleanAnswer:
             wordCount[word] += 1
 
 # mapping words with unique integer
-threshold = 20
-identDict = {}
-identNum = 0
+threshold_question = 20
+wordNum = 0
+questionDic = {}
 for word, count in wordCount.items():
-    if count >= threshold:
-        identDict[word] = identNum
-        identNum = identNum + 1
+    if count >= threshold_question:
+        questionDic[word] = wordNum
+        wordNum = wordNum + 1
+#print(list(questionDic.items()[:4]))
+print(questionDic[0])
+threshold_answer = 20
+answerDic = {}
+wordNum = 0
+for word, count in wordCount.items():
+    if count >= threshold_answer:
+        answerDic[word] = wordNum
+        wordNum = wordNum + 1
 
-questionDic = identDict
-answerDic = identDict
 
 # Extracting specifix keys from dictionary 
 def filter_key(dictionary, filterKey):
@@ -126,6 +132,8 @@ tokens = ['<PAD>', '<EOS>', '<OUT>', '<SOS>']
 
 for token in tokens:
     questionDic[token] = len(questionDic) + 1
+
+for token in tokens:
     answerDic[token] = len(answerDic) + 1
 
 
@@ -133,18 +141,16 @@ for token in tokens:
 answerDicInv = {w_i: w for w, w_i in answerDic.items()}
 
 # Adding end of string token to the end of sentence inside clean answer list
-endToken = ' <EOS>'
 for sentence in range(len(cleanAnswer)):
-    cleanAnswer[sentence] += endToken
+    cleanAnswer[sentence] += ' <EOS>'
 
 # Translating clean questions to integers
 questionInt = []
-outOfString = '<OUT>'
 for question in cleanQuestion:
     Int = []
     for word in question.split():
         if word not in questionDic:
-            Int.append(questionDic[outOfString])
+            Int.append(questionDic['<OUT>'])
         else:
             Int.append(questionDic[word])
     questionInt.append(Int)
@@ -154,7 +160,7 @@ for answer in cleanAnswer:
     Int = []
     for word in answer.split():
         if word not in answerDic:
-            Int.append(answerDic[outOfString])
+            Int.append(answerDic['<OUT>'])
         else:
             Int.append(answerDic[word])
     answerInt.append(Int)
@@ -172,10 +178,11 @@ for num in range(1, 26):
 ##### building sequence to sequence model #####
 
 # building functions for place holders
-def model_placeHolder:
+def model_placeHolder():
     inputs = tf.placeholder(tf.int32, [None, None], name = 'inputs')
     targets = tf.placeholder(tf.int32, [None, None], name = 'targets')
     learningRates = tf.placeholder(tf.int32, name = 'learningRates')
     keepProb = tf.placeholder(tf.int32, name = 'keepProb')
     return inputs, targets, learningRates, keepProb
-    
+
+#print(sortedAnswer[0:5])
